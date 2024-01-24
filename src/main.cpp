b48413f07fd916a981638b5afcc167f905728a27
abhinayh56 <kumarabhinay53@gmail.com>
2024-01-24 16:59:53 +0530

    added directory of harmonic notch filter

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include "Filter_LPF1.h"
+#include "Filter_LPF2.h"
 #include "Filter_HPF1.h"
+#include "Filter_HPF2.h"
+#include "Filter_BSF.h"
+#include "Filter_BPF.h"
+#include "Filter_SRF.h"
+#include "Filter_HNF.h"
 
 void setup(){
 }
